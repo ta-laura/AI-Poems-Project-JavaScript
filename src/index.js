c660d26e66aa66a52c3a05b687c836cbde19b5f1
ta-laura <laura.ta20@yahoo.de>
2025-02-27 13:40:59 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generatePoem(event) {
+    event.preventDefault();
+  
+    new Typewriter("#poem", {
+      strings: "When you smile, all the flowers in the world start blooming 🌹🌷🌻🪻",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
